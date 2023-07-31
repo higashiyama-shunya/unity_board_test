a239dbf341c218adaa22b41545a169bbc4e930a7
higashiyama-shunya <higashiyama.shunya@oysterworld.jp>
2023-07-31 18:13:45 +0900

    onClick(int index)追加
    自分の値としてitemIndexを持ちその値を使用してonClick(int index)が使えるように変更。値の取得などに関してはあんまり上手くいかなかったが、ボタンを押すとアイテムの値を使用してリストからデータを取得して表示されるように変更した。

--- ScrollItem.cs
+++ ScrollItem.cs
@@ -1,4 +1,5 @@
 using Mosframe;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,16 +20,60 @@ public class ScrollItem : MonoBehaviour, IDynamicScrollViewItem
     [SerializeField]
     public Image background;
 
+    private int itemIndex = -1;
+
+    [SerializeField]
+    DynamicVScrollView dynamicVScrollView;
+
+    /*
     public void OnClick()
     {
-        Dialog dialog;
+        //サンプルの方ではif文ではじいている
+        if (chatListResult == null) return;
+
+        //オブジェクトを取得するための変数
+        Text text;
+
         GameObject obj = transform.root.Find("Dialog").gameObject;
         obj.SetActive(true);
+        GameObject textObj = obj.transform.Find("Text").gameObject;
+        text = textObj.GetComponent<Text>();
+        Debug.Log("ボタンが押されました");
+        text.text = string.Format("ルームID:{0}\nルーム名:{1}\nオーナーID{2}", chatListResult.id, chatListResult.chat_room_name, chatListResult.owner_id);
+    }
+    */
+
+
+    public void onClick()
+    {
+        //初期値を
+        if (this.itemIndex == -1) return;
+        onClick(this.itemIndex);
+    }
+
+
+    public void onClick(int index)
+    {
+        Text text;
+        ScrollView scrollView;
+
+        GameObject obj = transform.root.Find("Dialog").gameObject;
+        GameObject textObj = obj.transform.Find("Text").gameObject;
+        text = textObj.GetComponent<Text>();
+
+        GameObject chatRoomList = transform.parent.parent.gameObject;
+        scrollView = chatRoomList.GetComponent<ScrollView>();
+
+        List<ChatListResult> chatListResultList = scrollView.list;
+        var data = chatListResultList[index];
+
+        text.text = string.Format("ID:{0}\nNAME:{1}\nオーナーID:{2}", data.id, data.chat_room_name, data.owner_id);
+        obj.SetActive(true);
+        Debug.Log(index + "番が押されました");
     }
 
     public void onUpdateItem(int index)
     {
-
         Debug.Log("onUpdateItem�J�n:" + index);
         ScrollView scrollView;
         GameObject obj = transform.parent.parent.gameObject;
@@ -36,14 +81,15 @@ public class ScrollItem : MonoBehaviour, IDynamicScrollViewItem
         List<ChatListResult> chatListResultList = scrollView.list;
         if (chatListResultList.Count > index)
         {
+            //最終選択番号を更新する。
+            this.itemIndex = index;
+
             var data = chatListResultList[index];
 
-            background.color = this.colors[Mathf.Abs(index) % colors.Length];
+            background.color = colors[Mathf.Abs(index) % colors.Length];
 
             title.text = string.Format("{0}:{1}", data.id, data.chat_room_name);
         }
         Debug.Log("onUpdateItem�I��:" + index);
-
     }
-
 }
